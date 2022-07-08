4153c0936ee90a4eb8af96dff736967d6f9194c4
cbrgm <chris@cbrgm.net>
2022-07-08 16:19:58 +0200

    feat: add a version overview to stdout

--- Dockerfiles/ubuntu/22.04/entrypoint.sh
+++ Dockerfiles/ubuntu/22.04/entrypoint.sh
@@ -3,33 +3,38 @@
 set -eu
 
 function setup_go_version {
-    # configure goenv version
-    echo "configuring goenv version..."
+    echo "configuring goenv to use go version $1..."
 
     export GOENV_ROOT="$HOME/.goenv"
     export PATH="$GOENV_ROOT/bin:$PATH"
     eval "$(goenv init -)"
 
-    goenv install $DEFAULT_GO_VERSION
-    goenv global $DEFAULT_GO_VERSION
-    echo "running $(go version)"
+    goenv install $1
+    goenv global $1
 }
 
 function setup_node_version {
-    echo "configuring npm version..."
+    echo "configuring nvm to use node version $1..."
 
     export NVM_DIR=$HOME/.nvm;
 
     source $NVM_DIR/nvm.sh;
-    nvm install --no-progress $DEFAULT_NODE_VERSION
-    nvm alias default $DEFAULT_NODE_VERSION
+    nvm install --no-progress $1
+    nvm alias default $1
     nvm use default
-
-    echo "running node version $(node -v)"
-    echo "running npm version $(npm -v)"
 }
 
-setup_go_version
-setup_node_version
+setup_go_version $DEFAULT_GO_VERSION
+setup_node_version $DEFAULT_NODE_VERSION
+
+echo ""
+echo "==============================================="
+echo "go version: $(go version)"
+echo "node version: $(node -v)"
+echo "npm version: $(npm -v)"
+echo "aws-cli version: $(aws --version)"
+echo "python3 version: $(python3 --version)"
+echo "==============================================="
+echo ""
 
 eval "$@"
