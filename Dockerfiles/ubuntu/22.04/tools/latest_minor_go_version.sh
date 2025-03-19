6e5876e69374b402f7720c51c710dccc493a26da
amir.vagapov <amir.vagapov@moia.io>
2025-03-19 18:16:21 +0100

    Remove dead code

--- Dockerfiles/ubuntu/22.04/tools/latest_minor_go_version.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-set -o pipefail
-
-major_version=$1
-
-"${HOME}"/.goenv/plugins/go-build/bin/go-build --definitions | grep "${major_version}" | sort --version-sort | tail -n1
